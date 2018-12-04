e679752beabee358701f8ae6f2c772cdf4e39991
William Pereira <william.pereira@me.com>
2018-12-03 22:06:47 -0200

    test for get specific ids

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const { ObjectID } = require('mongodb');
+
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
@@ -25,11 +27,31 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-	Todo.find().then((todos)=>{
-		res.send({todos})
-	}, (e) =>{
-		res.status(400).send(e);
-	})
+	Todo.find().then(
+		todos => {
+			res.send({ todos });
+		},
+		e => {
+			res.status(400).send(e);
+		}
+	);
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id)
+		.then(todo => {
+			if (!todo) {
+				return res.status(404).send('ID NOT FOUND');
+			}
+			res.send({todo});
+		})
+		.catch(e => res.status(404).send());
 });
 
 app.listen(3000, () => {
