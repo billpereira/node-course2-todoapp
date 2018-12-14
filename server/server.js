92eb697f81e050305a83cb166b0fbe1f85bccd2f
William Pereira <william.pereira@me.com>
2018-12-14 16:36:10 -0200

    added mlab uri

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var { User } = require('./models/user');
 
 var app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -51,9 +51,28 @@ app.get('/todos/:id', (req, res) => {
 			if (!todo) {
 				return res.status(404).send('ID NOT FOUND');
 			}
-			res.send({todo});
+			res.send({ todo });
 		})
 		.catch(e => res.status(404).send());
+});
+
+app.delete('todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndDelete(id)
+		.then(todo => {
+			if (!todo) {
+				return res.status(404).send();
+			}
+			res.send(todo);
+		})
+		.catch(e => {
+			res.status(400).send();
+		});
 });
 
 app.listen(port, () => {
