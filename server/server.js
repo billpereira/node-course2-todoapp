361154a9ec438993ff7bb90d05a617b55dd8b951
William Pereira <william.pereira@me.com>
2018-12-14 16:53:58 -0200

    fixed remove

--- server/server.js
+++ server/server.js
@@ -63,7 +63,7 @@ app.delete('todos/:id', (req, res) => {
 		return res.status(404).send();
 	}
 
-	Todo.findByIdAndDelete(id)
+	Todo.findByIdAndRemove(id)
 		.then(todo => {
 			if (!todo) {
 				return res.status(404).send();
