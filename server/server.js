4bef45c1731b219983ad7605d9906cf84641eb00
William Pereira <william.pereira@me.com>
2018-12-15 22:12:15 -0200

    added patch route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
@@ -8,7 +8,6 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
-
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -49,14 +48,17 @@ app.get('/todos/:id', (req, res) => {
 	Todo.findById(id)
 		.then(todo => {
 			if (!todo) {
-				return res.status(404).send('ID NOT FOUND');
+				return res.status(404).send();
 			}
+
 			res.send({ todo });
 		})
-		.catch(e => res.status(404).send());
+		.catch(e => {
+			res.status(400).send();
+		});
 });
 
-app.delete('todos/:id', (req, res) => {
+app.delete('/todos/:id', (req, res) => {
 	var id = req.params.id;
 
 	if (!ObjectID.isValid(id)) {
@@ -68,15 +70,43 @@ app.delete('todos/:id', (req, res) => {
 			if (!todo) {
 				return res.status(404).send();
 			}
-			res.send(todo);
+
+			res.send({ todo });
 		})
 		.catch(e => {
 			res.status(400).send();
 		});
 });
 
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+		.then(todo => {
+			if (!todo) {
+				return res.status(404).send();
+			}
+			res.send({ todo });
+		})
+		.catch(e => {
+			res.status(404).send();
+		});
+});
+
 app.listen(port, () => {
-	console.log(`Started on port ${port}`);
+	console.log(`Started up at port ${port}`);
 });
 
 module.exports = { app };
