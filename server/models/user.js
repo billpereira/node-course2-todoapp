3bcda667e298da682f6997d34fb22c5d8f862fb3
William Pereira <william.pereira@me.com>
2018-12-21 17:50:14 -0200

    Add post /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
 	email: {
@@ -57,6 +57,26 @@ UserSchema.methods.generateAuthToken = function() {
 	});
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+	var User = this;
+	return User.findOne({ email }).then((user) => {
+		if (!user) {
+			return Promise.reject();
+		}
+
+
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (err, res) => {
+				if (res) {
+					resolve(user);
+				} else {
+					reject();
+				}
+			});
+		});
+	});
+};
+
 UserSchema.statics.findByToken = function(token) {
 	var User = this;
 	var decoded;
@@ -77,18 +97,17 @@ UserSchema.statics.findByToken = function(token) {
 	});
 };
 
-UserSchema.pre('save',function (next){
+UserSchema.pre('save', function(next) {
 	var user = this;
-	if(user.isModified('password')){
-		bcrypt.genSalt(10,(err,salt)=>{
-			bcrypt.hash(user.password,salt,(err,hash)=>{
-				user.password = hash
-				next()
-			})
-		})
-	}
-	else next()
-})
+	if (user.isModified('password')) {
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash;
+				next();
+			});
+		});
+	} else next();
+});
 
 var User = mongoose.model('Users', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -64,6 +64,20 @@ app.get('/todos', (req, res) => {
 	);
 });
 
+app.post('/users/login', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+
+	User.findByCredentials(body.email, body.password)
+		.then(user => {
+			return user.generateAuthToken().then((token)=>{
+				res.header('x-auth', token).send(user);
+			})
+		})
+		.catch(e => {
+			res.status(400).send()
+		});
+});
+
 app.get('/todos/:id', (req, res) => {
 	var id = req.params.id;
 
